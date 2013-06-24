23346111c518fa1ca672f10ca7660323f52c4e36
Ingmar Aguiar <ingmar.aguiar@gmail.com>
2013-06-23 22:53:52 -0300

    Ajustes

--- src/br/com/furb/comp/gals/actions/Action_31.java
+++ src/br/com/furb/comp/gals/actions/Action_31.java
@@ -1,5 +1,6 @@
 package br.com.furb.comp.gals.actions;
 
+import br.com.furb.comp.gals.Modulo;
 import br.com.furb.comp.gals.RegistroSemantico;
 import br.com.furb.comp.gals.SemanticError;
 import br.com.furb.comp.gals.Token;
@@ -8,8 +9,23 @@ public class Action_31 implements IAction {
 
 	@Override
 	public void execute(RegistroSemantico rs, Token token) throws SemanticError {
-		// TODO Auto-generated method stub
-		
+		Modulo modulo = rs.getModuloAtual();
+
+		Token identificador = modulo.getIdentificadores().poll();
+		while (identificador != null) {
+			String lexeme = identificador.getLexeme();
+			int linha = identificador.getLine();
+			int posicao = identificador.getPosition();
+
+			if (!modulo.getSimbolos().containsKey(lexeme)) {
+				String mensagem = String.format("Erro na linha %d - identificador (%s) não declarado", linha, lexeme);
+				throw new SemanticError(mensagem, posicao, linha, identificador);
+			}
+
+			modulo.getCodigo().append(ESPACO).append(ESPACO).append("ldloc ").append(lexeme).append("\n");
+
+			identificador = modulo.getIdentificadores().poll();
+		}
 	}
 
 }
