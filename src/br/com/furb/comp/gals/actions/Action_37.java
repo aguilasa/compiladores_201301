2b93475610ac926482d1b294c16e37c8f43a7371
Ingmar Aguiar <ingmar.aguiar@gmail.com>
2013-06-30 15:33:09 -0300

    Ajustes

--- src/br/com/furb/comp/gals/actions/Action_37.java
+++ src/br/com/furb/comp/gals/actions/Action_37.java
@@ -8,8 +8,10 @@ public class Action_37 implements IAction {
 
 	@Override
 	public void execute(RegistroSemantico rs, Token token) throws SemanticError {
-		// TODO Auto-generated method stub
-		
+		if (!token.getTipoMSIL().equalsIgnoreCase("int64") || !token.getTipoMSIL().equalsIgnoreCase("float64")) {
+			String mensagem = String.format("Erro na linha %d - tipo incompatível (%s)", token.getLine(), token.getTipoMSIL());
+			throw new SemanticError(mensagem, token.getPosition(), token.getLine(), token);
+		}
 	}
 
 }
--- src/br/com/furb/comp/gals/actions/Action_38.java
+++ src/br/com/furb/comp/gals/actions/Action_38.java
@@ -1,5 +1,6 @@
 package br.com.furb.comp.gals.actions;
 
+import br.com.furb.comp.gals.Modulo;
 import br.com.furb.comp.gals.RegistroSemantico;
 import br.com.furb.comp.gals.SemanticError;
 import br.com.furb.comp.gals.Token;
@@ -8,8 +9,15 @@ public class Action_38 implements IAction {
 
 	@Override
 	public void execute(RegistroSemantico rs, Token token) throws SemanticError {
-		// TODO Auto-generated method stub
-		
+		if (!token.getTipoMSIL().equalsIgnoreCase("int64") || !token.getTipoMSIL().equalsIgnoreCase("float64")) {
+			String mensagem = String.format("Erro na linha %d - tipo incompatível (%s)", token.getLine(), token.getTipoMSIL());
+			throw new SemanticError(mensagem, token.getPosition(), token.getLine(), token);
+		}
+
+		Modulo modulo = rs.getModuloAtual();
+
+		modulo.getCodigo().append("ldc.i8 -1");
+		modulo.getCodigo().append("mul");
 	}
 
 }
